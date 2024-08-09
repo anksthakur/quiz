9bd4574ed532ff856f91f7f4906ce1281455f2da
anup thakur <anksthakur7@gmail.com>
2024-08-09 11:07:19 +0530

    middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,32 +4,31 @@ export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const cookies = req.cookies;
   const adminToken = cookies.get('authToken');
-  const userToken = cookies.get('next-auth.session-token');
+  const userToken = cookies.get('next-auth.session-token') || cookies.get('__Secure-next-auth.session-token');
+
+  console.log('Admin Token:', adminToken);
+console.log('User Token:', userToken);
 
   // Protect admin routes
   if (!adminToken && (pathname === '/admin' || pathname === '/adminquestions')) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  // // Protect user routes
+  // Protect user routes
   if (!userToken && pathname === '/user') {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  // authenticated as admin
+  // Admin is authenticated
   if (adminToken) {
-    if (pathname === '/user') {
-      return NextResponse.redirect(new URL("/admin", req.url));
-    } else if (pathname === '/login') {
+    if (pathname === '/user' || pathname === '/login') {
       return NextResponse.redirect(new URL("/admin", req.url));
     }
   }
 
-  // authenticated as user
+  // User is authenticated
   if (userToken) {
-    if (pathname === '/admin' || pathname === '/adminquestions') {
-      return NextResponse.redirect(new URL("/user", req.url));
-    } else if (pathname === '/login') {
+    if (pathname === '/admin' || pathname === '/adminquestions' || pathname === '/login') {
       return NextResponse.redirect(new URL("/user", req.url));
     }
   }
