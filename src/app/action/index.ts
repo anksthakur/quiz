b2c73046f313de9360fa58fe9bfc51103b099300
anup thakur <anksthakur7@gmail.com>
2024-08-09 10:13:12 +0530

    midd

--- src/app/action/index.ts
+++ src/app/action/index.ts
@@ -4,7 +4,7 @@ export async function doSocialLogin(formData: FormData) {
     const action = formData.get('action') as string;
     console.log(2)
     if (action) {
-        await signIn(action, { callbackUrl: "https://quiz-pearl-six.vercel.app/user" });
+        await signIn(action, { callbackUrl: "/user" });
         
     } else {
         throw new Error("No action specified for social login");
